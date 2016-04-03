3f5b4af746edf0391d455832a60ef815fd946b2c
Benjamin Knecht <ben@ygopro.de>
2016-04-03 19:11:32 +0200

    News Personen Controller update

--- src/DevPro/BackendBundle/Controller/NewsletterPersonenController.php
+++ src/DevPro/BackendBundle/Controller/NewsletterPersonenController.php
@@ -11,6 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use DevPro\BackendBundle\Entity\Newsletter;;
 use DevPro\BackendBundle\Form\Type\NewsletterType;
 use DevPro\BackendBundle\Newsletter\NewsletterManager;
+use DevPro\BackendBundle\Entity\NewsletterEmpfaenger;
 
 
 
@@ -18,6 +19,117 @@ use DevPro\BackendBundle\Newsletter\NewsletterManager;
 class NewsletterPersonenController extends Controller
 {
 
+    /**
+     * @Route("/admin/newsletter/personen", name="backend_newsletter_personen")
+     */
+    public function indexAction()
+    {
+
+        $data = $this->getDoctrine()
+            ->getRepository("DevProBackendBundle:NewsletterEmpfaenger")
+            ->findBy([], ['nachname' => 'ASC']);
+
+        $html = $this->container->get('templating')->render(
+            'Backend/Newsletter/Personen/index.html.twig', array(
+                "data" => $data
+            )
+        );
+
+        return new Response($html);
+    }
+
+
+    /**
+     * @Route("/admin/newsletter/personen/new", name="backend_newsletter_personen_new")
+     */
+    public function newAction(Request $request)
+    {
+        $data = new NewsletterEmpfaenger();
+        $form = $this->createForm(NewsletterType::class, $data);
+
+        $result = $this->handleFormUpload($form, $request, $data, 'neu');
+
+        if($result)
+        {
+            return $this->redirectToRoute('backend_newsletter_personen');
+        }
+
+        $html = $this->container->get('templating')->render(
+            'Backend/Newsletter/Personen/new.html.twig', array(
+                "data" => '',
+                "form" => $form->createView()
+            )
+        );
+
+        return new Response($html);
+    }
+
+    /**
+     * @Route("/admin/newslettter/personen/edit/{id}", name="backend_newsletter_personen_edit")
+     */
+    public function editAction(Request $request, $id)
+    {
+        $data = $this->getDoctrine()
+            ->getRepository('DevProBackendBundle:NewsletterEmpfaenger')
+            ->find($id);
+
+        $form = $this->createForm(NewsletterType::class, $data);
+
+        $result = $this->handleFormUpload($form, $request, $data, 'edit');
+        if($result)
+        {
+            return $this->redirectToRoute('backend_newsletter_personen');
+        }
+
+        $html = $this->container->get('templating')->render(
+            'Backend/Newsletter/Personen/edit.html.twig', array(
+                "form" => $form->createView()
+            )
+        );
+
+        return new Response($html);
+    }
+
+
+    /**
+     * @Route("/admin/newsletter/personen/delete/{id}", name="backend_newsletter_personen_delete")
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $data = $em
+            ->getRepository('DevProBackendBundle:NewsletterEmpfaenger')
+            ->find($id);
+
+        $em->remove($data);
+        $em->flush();
+
+        return $this->redirectToRoute('backend_newsletter_personen');
+    }
+
+
+
+    public function handleFormUpload($form, $request, $task, $action)
+    {
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+
+            $dataObject = $form->getData();
+
+            if($action == 'neu')
+            {
+                $sort = $this->getSetSort();
+                $sort++;
+                $dataObject->setSort($sort);
+            }
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($dataObject);
+            $em->flush();
+            return true;
+        }
+    }
 
 
 }
