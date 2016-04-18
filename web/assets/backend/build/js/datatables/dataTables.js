d1cd6680bceee5f3a25a9a2dcdecfaca8a3f21db
Selim Imoberdorf <selim@mac12.local>
2016-04-18 16:58:04 +0200

    AdminLTE Backend Skin Intergration
    – Integriert in Symfony
    – Frontend Package via bower
    – Grunt Task Runner für CSS & JS
    – composer Update

---
+++ web/assets/backend/build/js/datatables/dataTables.js
@@ -0,0 +1,31 @@
+
+// Blog / News
+$(document).ready(function() {
+
+    var table = $('#blogTable').DataTable({
+        "bSort" : true,
+        "bLengthChange": false,
+        "order": [[ 0, "desc" ]],
+        "language": {
+            "decimal":        "",
+            "emptyTable":     "Keine Daten vorhanden.",
+            "info":           "_START_ bis _END_ von _TOTAL_ Einträgen",
+            "infoEmpty":      "Keine Daten vorhanden",
+            "infoFiltered":   "in den Total _MAX_ Einträgen.",
+            "infoPostFix":    "",
+            "thousands":      "'",
+            "lengthMenu":     "_MENU_ Einträge anzeigen",
+            "loadingRecords": "Laden…",
+            "processing":     "Verarbeiten…",
+            "search":         "Suchen:",
+            "zeroRecords":    "Keine Resultate gefunden.",
+            "paginate": {
+                "sFirst": "Erste Seite",
+                "sLast": "Letzte Seite",
+                "sNext": "Nächste Seite",
+                "sPrevious": "Vorherige Seite"
+            }
+        }
+    });
+
+});
