6e798b9e63454c12d02f6aa7df06f59f25fd2636
Benjamin Knecht <bk@inter-punkt.ch>
2016-06-22 09:35:41 +0200

    Live Template Vorlagen

---
+++ liveTemplatesPHPStorm.php
@@ -0,0 +1,26 @@
+Live Templates für die Skeleton Anwendung & PHPStorm
+
+Controller
+    Main Actions inkl. Routen
+    - IndexAction -> Alle Daten aus der Entity an index.html.twig rendern
+    - insertAction -> Datensatz aus Form in DB
+    - updateAction -> Datensatz update
+    - deleteAction -> Datensatz löschen
+
+    Sub Functions
+    * render ->
+    * handleForm ->
+    * selectFindBy ->
+
+
+Entity
+
+
+
+FormType
+
+
+Twig
+
+<?php
+
