92dde797b1bd7c6b90e61e5ce89b61c81f542d60
Benjamin Knecht <bk@inter-punkt.ch>
2016-07-07 08:19:00 +0200

    Updater

--- src/DevPro/adminBundle/Controller/updateController.php
+++ src/DevPro/adminBundle/Controller/updateController.php
@@ -3,8 +3,10 @@ namespace DevPro\adminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use DevPro\adminBundle\Entity\update;
+use ZipArchive;
 
 class updateController extends Controller
 {
@@ -47,6 +49,9 @@ class updateController extends Controller
 
     }
 
+    //
+    // Update install Routine
+
     /**
      * @Route("/admin/update/install", name="admin_update_install")
      */
@@ -60,10 +65,33 @@ class updateController extends Controller
 
         $file = file_get_contents('http://' . $json->updateUrl);
 
+        $version++;
+        $fileDir = 'assets/updates/' . $version . '.zip';
+
         file_put_contents('assets/updates/' . $version . '.zip', $file);
 
+        $this->unzip($fileDir);
 
+        // Update DB set New Version in DB
+        $this->updateDatabaseVersion($version);
 
+        // Response Update success
+        return $this->redirectToRoute('admin_update_install_success', array('version' => $version));
+
+    }
+
+    /**
+     * @Route("/admin/update/install/success", name="admin_update_install_success")
+     */
+    public function updateInstallSuccessAction(Request $request)
+    {
+        $version = $request->query->get('version');
+        $html = $this->renderView('admin/update/installed.html.twig', array(
+            'title' => 'update',
+            'version' => $version
+        ));
+
+        return new Response($html);
     }
 
     protected function getCurrentVersion()
@@ -79,4 +107,34 @@ class updateController extends Controller
         return $data->getVersion();
     }
 
+    protected function unzip($file)
+    {
+        $rootDir = $this->get('kernel')->getRootDir() . '/../';
+
+        $zip = new ZipArchive;
+        if ($zip->open($file) === TRUE) {
+            $zip->extractTo($rootDir);
+            $zip->close();
+            echo 'ok';
+        } else {
+            echo 'Fehler';
+        }
+    }
+
+    protected function updateDatabaseVersion($version)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $data = $em->getRepository('DevProadminBundle:update')
+            ->find(1);
+
+        if ( ! $data)
+        {
+            throw $this->createNotFoundException('Update Entity, kein Datensatz mit ID 1 gefunden!');
+        }
+
+        $data->setVersion($version);
+
+        $em->persist($data);
+        $em->flush();
+    }
 }
