195f584ea7c114e2911ca06294fd5f412078afdc
Benjamin Knecht <bk@inter-punkt.ch>
2016-07-07 08:46:27 +0200

    remove Sonata Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,10 +26,6 @@ class AppKernel extends Kernel
             new DevPro\FrontendBundle\DevProFrontendBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-            new Sonata\MediaBundle\SonataMediaBundle(),
-            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-            new Sonata\IntlBundle\SonataIntlBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
