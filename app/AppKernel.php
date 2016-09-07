76614fe9f1c8184c5c16cdc9c4fbedd74625908d
Benjamin Knecht <bk@inter-punkt.ch>
2016-09-07 10:28:46 +0200

    Update Vichuploader and MediaManager

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new DevPro\interpunktBundle\DevProinterpunktBundle(),
             new FM\ElfinderBundle\FMElfinderBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new Maba\Bundle\WebpackBundle\MabaWebpackBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
