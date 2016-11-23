04606e73ec131c9ecf460f4328138995238ecfc5
Benjamin Knecht <bk@inter-punkt.ch>
2016-11-23 14:43:34 +0100

    [Mod] remove useless files

--- web/assets/admin/js/bootstrap-colorpicker/bootstrap-colorpicker.js
+++
@@ -1,9 +0,0 @@
-//  Bootstrap Colorpicker Configuration JS
-//  
-//  Copyright 2016 by inter-punkt.ag
-//  Autor: Selim Imoberdorf
-//  --------------------------------------------------------
-
-    $(function(){
-        $('.demo2').colorpicker();
-    });
