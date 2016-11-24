ff853927951e29dc2e84fd56a374a2a220389a61
Selim Imoberdorf <selim@mac12.local>
2016-11-24 09:09:04 +0100

    [MOD] total assets function finished
    – gulp task testetd > OK
    – compiled files are all ok
    – frontend template finished

---
+++ web/assets/frontend/js/main.js
@@ -0,0 +1,11 @@
+//  MAIN JS
+//  Copyright 2016 by inter-punkt.ag
+//  Autor: Selim Imoberdorf
+//  --------------------------------------------------------
+
+// Initial Webfont
+// WebFontConfig = {
+//     typekit: {
+//         id: 'rpt3igs'
+//     }
+// };
