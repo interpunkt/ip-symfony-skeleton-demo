5afcb50866d00aaaed97193f1c519f77901db953
Selim Imoberdorf <selim@mac12.local>
2016-11-29 16:29:00 +0100

    [NEW] status site and login
    – styling of status site finished
    – styling of login site finished

---
+++ web/assets/admin/js/ip-main.js
@@ -0,0 +1,11 @@
+//  MAIN JS
+//  Copyright 2016 by inter-punkt.ag
+//  Autor: Selim Imoberdorf
+//  --------------------------------------------------------
+
+// load google font
+WebFontConfig = {
+    google: {
+        families: ['Source+Sans+Pro:300,400,600,700,300i,400i,600i']
+    }
+};
